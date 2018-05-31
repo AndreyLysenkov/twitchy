ea2f0004983ff17c855f8dc4a2415c63ecdfd6fd
allan_walpy <andreylysenkovallan@gmail.com>
2018-05-31 18:03:45 +0300

    small fixes of cheer badge parser

--- src/parse/badge/cheer.js
+++ src/parse/badge/cheer.js
@@ -1,6 +1,6 @@
 function value_to_digit_array(conf, value) {
     let result = [];
-    for (; value > 0; value = Math.floor(conf.base)) {
+    for (; value > 0; value = Math.floor(value / conf.base)) {
         result.push(value % conf.base);
     }
     return result;
@@ -9,7 +9,7 @@ function value_to_digit_array(conf, value) {
 function value_shorten(conf, value) {
     let label = null;
     conf.shorten.forEach((x) => {
-        if ((value.lenth() > x.count) 
+        if ((value.length > x.count) 
             && ((!label) || (label.count < x.count)))
             label = x;
     });
@@ -17,7 +17,7 @@ function value_shorten(conf, value) {
     if (!label)
         return value;
     let new_value = [];
-    for (let i = 0; i < value.lenth() - 1; i++) {
+    for (let i = 0; i < value.length; i++) {
         if (i >= label.count)
             new_value.push(value[i]);
     }

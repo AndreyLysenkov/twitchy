5480b8cc6b37492f6b67e08b489df7793354964a
allan_walpy <andreylysenkovallan@gmail.com>
2018-06-01 19:27:12 +0300

    add logger to discord client

--- src/discord/bot.js
+++ src/discord/bot.js
@@ -5,8 +5,22 @@ const discord = require('discord.js');
 class DiscordBot {
 
     constructor(token) {
+        this.config = core.config.main.discord;
         this.token = token;
         this.client = new discord.Client();
+
+        this.set_logger();
+    }
+
+    set_logger() {
+        let self = this;
+        this.config.log.list.forEach((e) => {
+            self.client.on(e.event, (message) => DiscordBot.log(e.level, message));
+        });
+    }
+
+    static log(level, message) {
+        core.logger.send(level, this.config.log.module, message);
     }
 
     start() {

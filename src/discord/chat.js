14069fc1963ac41e8d1ad11b89eac534bc1659f8
allan_walpy <andreylysenkovallan@gmail.com>
2018-06-04 21:18:32 +0300

    add tmp support offlimit discord messages

--- src/discord/chat.js
+++ src/discord/chat.js
@@ -1,3 +1,6 @@
+const core = require('../core.js');
+const config = core.config.main.discord.chat;
+
 class Chat {
 
     constructor(client, guildId, channelId) {
@@ -8,9 +11,26 @@ class Chat {
         this.message_stringify = require('../stringify/core.js');
     }
 
+
+    limit_content(content) {
+        if (content.length <= config.limit)
+            return [ content ];
+
+        // TODO?;
+        let result = [];
+        for (let i = 0; i < content.length; i += config.limit) {
+            result.push(content.substring(i, i + config.limit));
+        }
+        return result;
+    }
+
     receive(message) {
         let content = this.message_stringify(message);
-        this.channel.send(content);
+        let lines = this.limit_content(content);
+        lines.forEach((line) => this.channel.send(
+            config.template.format({
+                "line": line
+            })));
     }
 
 }

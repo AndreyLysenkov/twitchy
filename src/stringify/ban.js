09fac1f18bfb84f1bc535b0bdd21a27eb4223e22
allan_walpy <andreylysenkovallan@gmail.com>
2018-06-05 12:38:16 +0300

    add stringify timeout event

--- src/stringify/ban.js
+++ src/stringify/ban.js
@@ -22,12 +22,21 @@ class ActionEvent {
         return this.entry.data.argument[1];
     }
 
+    parse_duration() {
+        let time = this.entry.data.argument[3];
+        if (!time)
+            return null;
+        // TODO;
+        return time;
+    }
+
     parse(entry) {
         this.entry = entry;
         
         this.time = this.parse_time();
         this.username = this.parse_username();
         this.reason = this.parse_reason();
+        this.duration = this.parse_duration();
 
         return this.config.template.format(this);
     }

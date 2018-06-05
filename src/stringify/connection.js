0155c5d8b2532daed181f6650c73784ed2a886de
allan_walpy <andreylysenkovallan@gmail.com>
2018-06-05 12:18:44 +0300

    add conection events

---
+++ src/stringify/connection.js
@@ -0,0 +1,37 @@
+const core = require('../core.js');
+const tz = require('moment-timezone');
+
+class ActionEvent {
+
+    constructor(config) {
+        this.config = config;
+    }
+
+    parse_time() {
+        let time = this.entry.data.time;
+        return time
+            .tz(core.config.app.time.zone)
+            .format(core.config.app.time.format);
+    }
+
+    parse_adress() {
+        return this.entry.data.argument[1];
+    }
+
+    parse_port() {
+        return this.entry.data.argument[2];
+    }
+
+    parse(entry) {
+        this.entry = entry;
+        
+        this.time = this.parse_time();
+        this.adress = this.parse_adress();
+        this.port = this.parse_port();
+
+        return this.config.template.format(this);
+    }
+
+}
+
+module.exports = ActionEvent;

d1ce825a68db4c2ab6e28f6d6e962b97876f9192
allan_walpy <andreylysenkovallan@gmail.com>
2018-06-09 01:24:04 +0300

    add bot owner filter to ./discord/command/reboot

--- src/discord/command/reboot.js
+++ src/discord/command/reboot.js
@@ -1,10 +1,16 @@
+const core = require('../../core.js');
+const owners = core.config.private.owners;
+
 class RebootCommand {
 
     static kill() {
-        process.exit(1);
+        process.exit(0);
     }
 
     static call(data) {
+        let authorId = data.message.author.id;
+        if (!owners.includes(authorId))
+            return;
         data.channel.send(data.config.reply.format(data)).then(() => {
             RebootCommand.kill();
         });

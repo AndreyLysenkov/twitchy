c90e44d3170425cb890c13e208af5fe890bcf313
allan_walpy <andreylysenkovallan@gmail.com>
2018-06-08 21:55:22 +0300

    add show actual emojis in list for ./discord/command/emojie/show

--- src/discord/command/emojie/show.js
+++ src/discord/command/emojie/show.js
@@ -3,12 +3,52 @@ const guilds_config = core.app.bot.discord.guild;
 
 class EmojiShowSubcommand {
 
+    static getEmojie(list, id) {
+        return list.get(id).toString();
+    }
+
+    static getEmojis(guild) {
+        return guild.emojis;
+    }
+
+    static getList(list, config_list, isInList) {
+        let result = [];
+        list.forEach((emojie) => {
+            let isIncludes = config_list.includes(emojie.id);
+            if ((isInList && isIncludes)
+                || (!isInList && !isIncludes))
+                result.push(emojie);
+        });
+        return result;
+    }
+
+    static toSting(config, list) {
+        list.forEach(emojie => emojie.toSting());
+        return list.join(config.separator);
+    }
+
     static call(data) {
         let guild_config = guilds_config[data.channel.guild.id];
         if (!guild_config)
             return;
 
         data.emojie_setting = JSON.stringify(guild_config.config.emojie, null, data.config.json_option);
+        let all_emojis = EmojiShowSubcommand.getEmojis(data.channel.guild);
+        data.emojie_list_in = EmojiShowSubcommand.toSting(
+            data.config,
+            EmojiShowSubcommand.getList(
+                all_emojis,
+                guild_config.config.emojie.list,
+                true
+            ));
+        data.emojie_list_out = EmojiShowSubcommand.toSting(
+            data.config,
+            EmojiShowSubcommand.getList(
+                all_emojis,
+                guild_config.config.emojie.list,
+                false
+            ));
+
         data.channel.send(data.config.reply.format(data));
     }
 
